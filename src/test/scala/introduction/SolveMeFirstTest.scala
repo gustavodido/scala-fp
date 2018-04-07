159373f34ed50449938d91b029a16ef601f1b50e
Gu1f3f3 <gustavo_di_domenico@gap.com>
2018-04-06 23:15:03 -0700

    Refactor tests.

--- src/test/scala/introduction/SolveMeFirstTest.scala
+++ src/test/scala/introduction/SolveMeFirstTest.scala
@@ -1,16 +1,12 @@
 package introduction
 
-import java.io.{ByteArrayInputStream, ByteArrayOutputStream}
-import java.lang.System.setIn
-import java.nio.charset.StandardCharsets.UTF_8
-
 import common.InOutTest
-import org.scalatest.FlatSpec
-
-import scala.Console.withOut
 
 class SolveMeFirstTest extends InOutTest {
   "SolveMeFirst" should "sum two regular numbers" in {
-    assertExercise(new SolveMeFirst, "1\n4\n", "5\n")
+    val input = "1\n4\n"
+    val output = "5\n"
+
+    assertExercise(new SolveMeFirst, input, output)
   }
 }
